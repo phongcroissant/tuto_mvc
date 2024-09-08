3817e5fdbb86b9822038dba53d0282d93532722b
jeanbaptisteaubry <jean-baptiste.aubry@ac-besancon.fr>
2024-09-09 01:35:05 +0200

    Avec l'objet vue et message

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once "vendor/autoload.php";
 use App\Utilitaire\Vue;
+use App\Utilitaire\Vue_AfficherMessage;
 use App\Vue\Vue_Bienvenue;
 
 if(isset($_REQUEST["case"]))
@@ -12,6 +13,7 @@ $Vue = new Vue();
 switch ($case) {
     case "defaut":
         $Vue->addToCorps(new Vue_Bienvenue());
+        $Vue->addToCorps(new Vue_AfficherMessage("et j'ajoute un message !"));
         break;
 
 }
